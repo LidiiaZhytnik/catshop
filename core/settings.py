6daa4b4278243a77a2fd1038b47ffcdd8ed4061f
Lida <lida.zhitnik@gmail.com>
2021-11-21 00:04:40 +0200

    creation of URLs, Views, Bootstrap templates

--- core/settings.py
+++ core/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'store.views.categories',
             ],
         },
     },
--- store/views.py
+++ store/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Category, Animal
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
+def all_animals(request):
+    animals = Animal.objects.all()
+    return render(request, 'store/home.html', {'animals': animals})
